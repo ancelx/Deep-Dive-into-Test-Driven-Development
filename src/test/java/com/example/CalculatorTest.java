0a25c75d06dbd6baaf25ab35b7cfc7fbade1d43d
Ancel Almeida <ancel@appic.me>
2023-03-10 19:17:04 +0530

    init

---
+++ src/test/java/com/example/CalculatorTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class CalculatorTest {
+
+@Test
+    public void testAddition() {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(2, 3);
+        assertEquals(5, result);
+    }
+}
